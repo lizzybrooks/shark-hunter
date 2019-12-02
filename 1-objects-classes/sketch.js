ce6208fbac3be0443e0b9f3de3fe7d59079dca5b
Lizzy Brooks <lizzy.brooks@gmail.com>
2019-12-02 13:16:31 -0800

    debugged the spear (sort of)

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -4,50 +4,63 @@ let spearX;
 let spearY;
 
 
-function box1(x){
-  // noStroke();
-  fill(220);
-  rect(100,100,this.x+x,40);
-}
-function box2(x){
-  // noStroke();
-  fill(220);
-  rect(500,300,this.x-10+x,50);
-}
-function mouseClicked(){
-  print(mouseX, mouseY);
-  spearX = mouseX;
-  spearY = mouseY;
-}
+
 function setup() {
   createCanvas(800, 400);
-  spear = new Spear(100, 100,"brown"); //make a new ball from the Ball class and call it b.
+  spear = new Spear(100, 100,100,100,"brown", false); //make a new ball from the Ball class and call it b.
 }
 
 function draw(){
-	background(220);
-    spear.drawSpear(); //draw the ball called b (go look in the Ball class for the drawBall function)
-    spear.moveSpear(); //move the ball called b (go look in the Ball class for the moveBall function)
+  background(220);
+
+  spear.drawSpearTrajectory(); //draw the ball called b (go look in the Ball class for the drawBall function)
+  spear.drawCover();
+  spear.moveSpear();
 }
 
 
-//ball class from which to create new balls with similar properties.
+function mouseClicked(){
+  print(mouseX, mouseY);
+  spear.spearX = mouseX;
+  spear.spearY = mouseY;
+
+
+}
+
+function keyPressed(){
+ spear.moving = true;
+
+}
+
+
 class Spear {
 
-	constructor(x,y,color){ //every ball needs an x value and a y value
-		    this.x = x;
+	constructor(x,y,endX,endY,color,moving){ //every ball needs an x value and a y value
+		this.x = x;
     		this.y = y;
-        this.color = color;
+       		this.spearX = endX;
+      		this.speary = endY;
+        	this.color = color;
+        	this.moving = moving;
 	}
-	drawSpear(){  // draw a ball on the screen at x,y
+
+	drawSpearTrajectory(){  // draw a ball on the screen at x,y
     		stroke(10);
     		fill(this.color);
-
-        strokeWeight(3);
-        line(this.x,this.y,spearX,spearY);
+       		strokeWeight(3);
+        	line(this.x,this.y,this.spearX,this.spearY);
 	}
-	moveSpear(){ //update the location of the ball, so it moves across the screen
-    box1(this.x+1);
-    box2(this.x+1);
+
+  	drawCover(){
+    		stroke(220)
+    		strokeWeight(30)
+    		line(this.x+50,this.y+5, this.spearX, this.spearY)
+
+  	}
+	
+	moveSpear(){
+    		if(this.moving == true && this.x < this.spearX){
+        	this.x = this.x+ 3
+    		}
 	}
 }
