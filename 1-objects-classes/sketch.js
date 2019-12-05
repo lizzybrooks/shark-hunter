ac6d1cb256e409d229e4fac25a580e293d5a5037
finmanis <51968394+finmanis@users.noreply.github.com>
2019-12-04 20:59:01 -0800

    Update sketch.js

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -7,7 +7,7 @@ let spearY;
 
 function setup() {
   createCanvas(800, 400);
-  spear = new Spear(100,100,100,100,100,100,"brown", true); //make a new ball from the Ball class and call it b.
+  spear = new Spear(100,100,100,100,100,100,100,100,"brown", false); //make a new ball from the Ball class and call it b.
 }
 
 function draw(){
@@ -15,6 +15,7 @@ function draw(){
 
   spear.drawSpearTrajectory(); //draw the ball called b (go look in the Ball class for the drawBall function)
   spear.drawCover1();
+  spear.drawCover2();
   spear.moveSpear();
 }
 
@@ -31,34 +32,45 @@ function mouseClicked(){
 
 class Spear {
 
-	constructor(x,y,x1,y1,endX,endY,color,moving){ //every ball needs an x value and a y value
+	constructor(x,y,x1,y1,endX,endY,endX1,endY1,color,moving){ //every ball needs an x value and a y value
 		this.x = x;
     		this.y = y;
         this.x1 = x1;
         this.y1 = y1;
        		this.spearX = endX;
-      		this.speary = endY;
+      		this.spearY = endY;
+          this.spearX1 = endX1;
+          this.spearY1 = endY1;
         	this.color = color;
         	this.moving = moving;
 	}
 
 	drawSpearTrajectory(){  // draw a ball on the screen at x,y
-    		stroke(10);
-    		fill(this.color);
-       		strokeWeight(3);
-        	line(this.x,this.y,this.spearX,this.spearY);
+    if(spear.moving==true){
+      stroke(10);
+      fill(this.color);
+        strokeWeight(3);
+        line(this.x,this.y,this.spearX,this.spearY);
+    }
+
 	}
 
   	drawCover1(){
-    	rect(this.x1,this.y1,this.x1+40,this.y1+40)
-
+      if(spear.moving==true){
+            	rect(this.x,this.y,this.x1+10,this.y1+20)
+      }
   	}
+    drawCover2(){
+      if(spear.moving==true){
+          rect(this.x+60,this.y,this.spearX,this.spearY)
+      }
+    }
 
 	moveSpear(){
     		if(this.moving == true){
         	this.x1 = this.x1+3
-          // this.spearX = this.spearX+3
 
-    		}
+
+    		 }
 	}
-}
+ }
